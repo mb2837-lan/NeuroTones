9651430251175a451c6271461d6d8a9188ba41bc
Matt Bauer <MB2837@lanvera.org>
2025-06-25 13:04:12 -0500

    add brown noise generator

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ namespace BinauralBeats
         private NumericUpDown _beatFreqInput;
         private Button _startButton;
         private Button _stopButton;
+        private Button _brownNoiseButton;
         private AudioEngine _engine;
 
         public Form1()
@@ -14,21 +15,24 @@ namespace BinauralBeats
 
             Text = "Binaural Beats";
             Width = 300;
-            Height = 200;
+            Height = 250;
 
             _baseFreqInput = new NumericUpDown { Minimum = 100, Maximum = 1000, Value = 200, Location = new System.Drawing.Point(10, 10) };
             _beatFreqInput = new NumericUpDown { Minimum = 1, Maximum = 30, Value = 7, Location = new System.Drawing.Point(10, 40) };
 
             _startButton = new Button { Text = "Start", Location = new System.Drawing.Point(10, 80) };
             _stopButton = new Button { Text = "Stop", Location = new System.Drawing.Point(100, 80) };
+            _brownNoiseButton = new Button { Text = "Brown Noise", Location = new System.Drawing.Point(10, 120) };
 
             _startButton.Click += (s, e) => _engine.Start((float)_baseFreqInput.Value, (float)_beatFreqInput.Value);
             _stopButton.Click += (s, e) => _engine.Stop();
+            _brownNoiseButton.Click += (s, e) => _engine.StartBrownNoise();
 
             Controls.Add(_baseFreqInput);
             Controls.Add(_beatFreqInput);
             Controls.Add(_startButton);
             Controls.Add(_stopButton);
+            Controls.Add(_brownNoiseButton);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -50,15 +50,37 @@ namespace BinauralBeats
         }
     }
 
+    public class BrownNoiseProvider : WaveProvider32
+    {
+        private Random _rand = new Random();
+        private float _lastOut = 0;
+
+        public BrownNoiseProvider()
+        {
+            SetWaveFormat(44100, 1); // Mono
+        }
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            for (int i = 0; i < sampleCount; i++)
+            {
+                float white = (float)(_rand.NextDouble() * 2.0 - 1.0);
+                _lastOut = (_lastOut + (0.02f * white)) / 1.02f;
+                buffer[offset + i] = _lastOut * 3.5f;
+            }
+            return sampleCount;
+        }
+    }
+
     public class AudioEngine
     {
         private WaveOutEvent _waveOut;
-        private BinauralWaveProvider _provider;
+        private IWaveProvider _provider;
 
         public void Start(float baseFreq, float beatFreq)
         {
-            Stop(); // Ensure previous playback is stopped before starting new one
-
+            Stop();
+            
             float left = baseFreq;
             float right = baseFreq + beatFreq;
 
@@ -68,26 +90,21 @@ namespace BinauralBeats
             _waveOut.Play();
         }
 
+        public void StartBrownNoise()
+        {
+            Stop();
+            
+            _provider = new BrownNoiseProvider();
+            _waveOut = new WaveOutEvent();
+            _waveOut.Init(_provider);
+            _waveOut.Play();
+        }
+
         public void Stop()
         {
-            if (_waveOut != null)
-            {
-                try
-                {
-                    _waveOut.Stop();
-                }
-                catch
-                {
-                    // Log or ignore depending on your logging strategy
-                }
-                finally
-                {
-                    _waveOut.Dispose();
-                    _waveOut = null;
-                }
-            }
-
-            _provider = null;
+            _waveOut?.Stop();
+            _waveOut?.Dispose();
+            _waveOut = null;
         }
     }
 }
